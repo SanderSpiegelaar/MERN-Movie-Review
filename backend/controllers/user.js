549bc0cda0daf376f8cad6304acea0a60cfb1b89
Sander Spiegelaar <sanderspiegelaar@gmail.com>
2023-03-19 11:57:53 +0100

    initial

---
+++ backend/controllers/user.js
@@ -0,0 +1,4 @@
+exports.createUser = (req, res) => {
+    res.header("type", "text/html");
+    res.send('<h1>This is the create user controller</h1>');
+}
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const userRouter = require('./routes/user');
 
-const app = express();
+const server = express();
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+server.use(userRouter);
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000 test');
 });
